8fb1eac5f4a665a19346a267ef0b8da78166bc59
felipeafbruno <felipedobru@gmail.com>
2019-07-31 14:37:15 -0300

    Site de Ecommerce desenvolvido durante o curso de PHP da Hcode

--- vendor/php-classes/src/Model/Order.php
+++ vendor/php-classes/src/Model/Order.php
@@ -39,12 +39,12 @@
 
 			$results = $sql->select("
 				SELECT *
-				FROM tb_orders a 
-				INNER JOIN tb_ordersstatus b USING(idstatus)
-				INNER JOIN tb_carts c USING(idcart)
-				INNER JOIN tb_users d ON d.iduser = a.iduser
-				INNER JOIN tb_addresses e USING(idaddress)
-				INNER JOIN tb_persons f ON f.idperson = d.idperson
+					FROM tb_orders a 
+					INNER JOIN tb_ordersstatus b USING(idstatus)
+					INNER JOIN tb_carts c USING(idcart)
+					INNER JOIN tb_users d ON d.iduser = a.iduser
+					INNER JOIN tb_addresses e USING(idaddress)
+					INNER JOIN tb_persons f ON f.idperson = d.idperson
 				WHERE a.idorder = :idorder
 				", [
 
@@ -141,6 +141,71 @@
 		public static function clearMsgSuccess() {
 
 			$_SESSION[Order::SUCCESS] = NULL;
+
+		}
+
+		public static function getPage($page = 1, $itemsPerPage = 10) {
+
+			//calculo para saber em qual número de página esta os items.
+			$start = ($page - 1) * $itemsPerPage;
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT SQL_CALC_FOUND_ROWS * 
+				FROM tb_orders a 
+				INNER JOIN tb_ordersstatus b USING(idstatus)
+				INNER JOIN tb_carts c USING(idcart)
+				INNER JOIN tb_users d ON d.iduser = a.iduser
+				INNER JOIN tb_addresses e USING(idaddress)
+				INNER JOIN tb_persons f ON f.idperson = d.idperson
+				ORDER BY a.dtregister DESC
+				LIMIT $start, $itemsPerPage;
+			");
+
+			$resultTotal = $sql->select("
+				SELECT FOUND_ROWS() AS nrtotal;
+			");
+
+			return [
+				'data'=>$results,
+				'total'=>(int)$resultTotal[0]["nrtotal"],
+				'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+			];
+
+		}
+
+		public static function getPageSearch($search, $page = 1, $itemsPerPage = 10) {
+
+			//calculo para saber em qual número de página esta os items.
+			$start = ($page - 1) * $itemsPerPage;
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT SQL_CALC_FOUND_ROWS * 
+				FROM tb_orders a 
+				INNER JOIN tb_ordersstatus b USING(idstatus)
+				INNER JOIN tb_carts c USING(idcart)
+				INNER JOIN tb_users d ON d.iduser = a.iduser
+				INNER JOIN tb_addresses e USING(idaddress)
+				INNER JOIN tb_persons f ON f.idperson = d.idperson
+				WHERE a.idorder = :idorder OR f.desperson LIKE :search
+				LIMIT $start, $itemsPerPage;
+			", [
+				':search'=>'%'.$search.'%',
+				':id'=>$search
+			]);
+
+			$resultTotal = $sql->select("
+				SELECT FOUND_ROWS() AS nrtotal;
+			");
+
+			return [
+				'data'=>$results,
+				'total'=>(int)$resultTotal[0]["nrtotal"],
+				'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+			];
 
 		}
 
